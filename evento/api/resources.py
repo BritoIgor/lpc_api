1d9c998120d7c2609c6271a089e7e6d8a9fe41e9
Linguagem de Programação Comercial <lpc.pmw@labin.canoas>
2017-04-17 21:44:55 -0300

    'commit_2'

--- evento/api/resources.py
+++ evento/api/resources.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from evento.models import TipoInscricao
 from django.contrib.auth.models import User
+from evento.models import Pessoa
 
 
 class TipoInscricaoResource(ModelResource):
@@ -13,3 +14,8 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+
+class PessoaResource(ModelResource):
+    class Meta:
+        queryset = Pessoa.objects.all()
+        allowed_methods = ['get']
